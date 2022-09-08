46dcdc25356df09930b43f041f5be02752fa8015
Homayoon Ranjbar <horanj.1022@gmail.com>
2022-09-07 22:45:12 -0500

    Create pilot.py

---
+++ pilot.py
@@ -0,0 +1,44 @@
+# import libraries (you may add additional imports but you may not have to)
+import numpy as np
+import pandas as pd
+from scipy.sparse import csr_matrix
+from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
+
+# get data files
+!wget https://cdn.freecodecamp.org/project-data/books/book-crossings.zip
+
+!unzip book-crossings.zip
+
+books_filename = 'BX-Books.csv'
+ratings_filename = 'BX-Book-Ratings.csv'
+
+# import csv data into dataframes
+df_books = pd.read_csv(
+    books_filename,
+    encoding = "ISO-8859-1",
+    sep=";",
+    header=0,
+    names=['isbn', 'title', 'author'],
+    usecols=['isbn', 'title', 'author'],
+    dtype={'isbn': 'str', 'title': 'str', 'author': 'str'})
+
+df_ratings = pd.read_csv(
+    ratings_filename,
+    encoding = "ISO-8859-1",
+    sep=";",
+    header=0,
+    names=['user', 'isbn', 'rating'],
+    usecols=['user', 'isbn', 'rating'],
+    dtype={'user': 'int32', 'isbn': 'str', 'rating': 'float32'})
+
+user_ratingCount = (df_ratings.
+     groupby(by = ['user'])['rating'].
+     count().
+     reset_index().
+     rename(columns = {'rating': 'totalRatingCount'})
+     [['user', 'totalRatingCount']]
+)
+users_to_keep = user_ratingCount.query('totalRatingCount > 200').user.tolist()
+
+df = pd.merge(df_books, df_ratings, on='isbn')
